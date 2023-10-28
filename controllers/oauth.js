aaba2fd2a4dff174dbbd2f2a67fe801a0543f0e3
Nina Saunders <kiyoos@buyi.edu>
2023-10-28 11:56:41 -0700

    Lesson7 - Oauth Part 2

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const oauthConnect = async (req, res) => {
   try {
     res.redirect(
@@ -8,6 +10,67 @@ const oauthConnect = async (req, res) => {
   }
 };
 
+/*
+const oauthCallback = async (req, res) => {
+  axios({
+    method: 'POST',
+    url: `https://github.com/login/oauth/access_token?client_id=${process.env.githubClientId}&client_secret=${process.env.githubSecret}&code=${req.query.code}`,
+    headers: { Accept: 'application/json' }
+  }).then((response) => {
+    res.redirect(`http://localhost:8080?access_token=${response.data.access_token}`);
+  });
+};
+*/
+/*
+const oauthCallback = async (req, res) => {
+  try {
+    const requestToken = req.query.code;
+    axios({
+      method: 'post',
+      url: `https://github.com/login/oauth/access_token?client_id=${process.env.githubClientId}&client_secret=${process.env.githubSecret}&code=${requestToken}`,
+      headers: { accept: 'application/json' }
+    }).then((res) => {
+      const access_token = res.data.access_token;
+      res.redirect('/success');
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const oauthSuccess = async (req, res) => {
+  axios({
+    method: 'get',
+    url: `https://api.github.com/user`,
+    headers: {
+      Authorization: 'token ' + access_token
+    }
+  }).then((res) => {
+    res.render('/success', { userData: res.data });
+  });
+};
+*/
+/*
+const oauthCallback = async ({ query: { code } }, res) => {
+  const body = {
+    clientId: process.env.githubClientId,
+    clientSecret: process.env.githubSecret,
+    code
+  };
+  console.log(body);
+  const opts = { headers: { accept: 'application/json' } };
+  axios
+    .post('https://github.com/login/oauth/access_token', body, opts)
+    .then((_res) => _res.data.access_token)
+    .then((token) => {
+      console.log('My token:', token);
+      res.redirect(`/?token=${token}`);
+    })
+    .catch((err) => res.status(500).json({ err: err.message }));
+};
+*/
 module.exports = {
   oauthConnect
+  // oauthCallback
+  // oauthSuccess
 };
--- routes/oauth.js
+++ routes/oauth.js
@@ -2,8 +2,30 @@ const express = require('express');
 const router = express.Router();
 const oauthController = require('../controllers/oauth');
 // const validation = require('../middleware/validate');
+const axios = require('axios');
 
 router.get('/', oauthController.oauthConnect);
-router.get('/oauth-callback', oauthController.oauthConnect);
+// router.get('/oauth-callback?code=', oauthController.oauthCallback);
+
+// router.get('/success', oauthController.oauthSuccess);
+/*
+router.get('/oauth-callback', ({ query: { code } }, res) => {
+  const body = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_SECRET,
+    code
+  };
+  const opts = { headers: { accept: 'application/json' } };
+  axios
+    .post('https://github.com/login/oauth/access_token', body, opts)
+    .then((_res) => _res.data.access_token)
+    .then((token) => {
+      // eslint-disable-next-line no-console
+      console.log('My token:', token);
+
+      res.redirect(`/?token=${token}`);
+    })
+    .catch((err) => res.status(500).json({ err: err.message }));
+});*/
 
 module.exports = router;
