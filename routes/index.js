ea9e6c6fa052f9dc3e427adf0d582f19dc5048fb
Nina Saunders <kiyoos@buyi.edu>
2023-11-04 09:55:51 -0700

    Lesson8 - Auth0 updates

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require('express-openid-connect');
+const auth0Controller = require('../controllers/auth0');
 
 const home = require('./home');
 const auth0 = require('./auth0');
@@ -10,6 +12,7 @@ const profile = require('./profile');
 const swagger = require('./swagger');
 
 router.use('/', home);
+router.use(auth(auth0Controller.config));
 router.use('/auth0', auth0);
 router.use('/fish', fish);
 router.use('/hikes', hikes);
