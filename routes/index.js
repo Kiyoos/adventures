6aa23679466e9d286c0b84086079967cddcccc61
Nina Saunders <kiyoos@buyi.edu>
2023-11-04 12:57:26 -0700

    Lesson8 - Disabling GitHub OAuth

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const home = require('./home');
 const auth0 = require('./auth0');
 const fish = require('./fish');
 const hikes = require('./hikes');
-const oauth = require('./oauth');
+// const oauth = require('./oauth');
 const profile = require('./profile');
 const swagger = require('./swagger');
 
@@ -16,8 +16,8 @@ router.use(auth(auth0Controller.config));
 router.use('/auth0', auth0);
 router.use('/fish', fish);
 router.use('/hikes', hikes);
-router.use('/oauth', oauth);
-router.use('/callback', oauth);
+// router.use('/oauth', oauth);
+// router.use('/callback', oauth);
 router.use('/profile', profile);
 router.use('/swagger', swagger);
 
